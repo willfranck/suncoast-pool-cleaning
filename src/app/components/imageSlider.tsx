54f369237c39b4cf6129aa6b0919399d18bc475e
willfranck <jspec520@gmail.com>
2023-11-27 21:02:48 -0500

    Fix: Reverted to pagination for slider | Adjusted params

--- src/app/components/imageSlider.tsx
+++ src/app/components/imageSlider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, EffectCoverflow, Autoplay } from 'swiper/modules'
+import { EffectCoverflow, Pagination, Autoplay } from 'swiper/modules'
 import 'swiper/css/bundle'
 import content from '@data/content'
 
@@ -13,23 +13,33 @@ export default function ImageSlider() {
   return (
     <article className='max-w-[60rem] py-20 overflow-hidden'>
       <div className='flex flex-col items-center md:items-start px-6'>
-        <p className='w-full md:w-1/2 pb-1'>{content.slider.subtitle}</p>
+        <p className='w-full md:w-1/2 pb-1'>
+          {content.slider.subtitle}
+        </p>
         <hr className='w-full md:w-1/2 pb-6 border-t-1 border-gray-500'></hr>
       </div>
 
       <Swiper
-        modules={[ Navigation, EffectCoverflow, Autoplay ]}
+        modules={[ EffectCoverflow, Pagination, Autoplay ]}
         effect='coverflow'
         coverflowEffect={{
           rotate: 30,
           depth: 0,
           scale: 0.9,
         }}
-        navigation
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
         grabCursor={true}
         initialSlide={1}
         slidesPerView={3}
         spaceBetween={0}
+        loop={true}
+        loopAdditionalSlides={5}
+        autoplay={{
+          delay: 4000,
+        }}
         breakpoints={{
           768: {
             spaceBetween: 30,
@@ -38,20 +48,14 @@ export default function ImageSlider() {
             },
           },
           1200: {
-            slidesPerView: 5,
             coverflowEffect: {
               rotate: 15,
               depth: 100,
+              stretch: 30,
               scale: 1,
             },
           },
         }}
-        loop={true}
-        loopAdditionalSlides={3}
-        autoplay={{
-          delay: 4000,
-        }}
-        className='rounded-sm'
       >
         {sliderImages && sliderImages.map((image, index) => (
           <SwiperSlide key={index}>
