7eb089b07983cf3a7e1d2089401b66c0ec00e086
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 12:58:20 -0500

    Fix: Added width/height to pseudo element

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <article className='relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem] before:absolute before:content-[""] before:inset-0 before:bg-banner before:bg-center before:bg-cover before:bg-fixed before:will-change-scroll'>
+    <article className='relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem] before:absolute before:content-[""] before:inset-0 before:w-full before:h-full before:bg-banner before:bg-center before:bg-cover before:bg-fixed'>
       
     </article>
   )
