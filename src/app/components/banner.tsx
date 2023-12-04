211267f3b63996e9f7c18963fb3929f239a25dea
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 13:41:43 -0500

    Removed text | Added will-change for iOS

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <article className='banner relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem]'>
-      <h3>TEST</h3>
+      
     </article>
   )
 }
