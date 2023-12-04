a9ce0061df2bfe561424c381958e43eaebcb47d9
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 12:55:06 -0500

    Fix: Placed background in pseudo element

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <article className='w-full h-[35rem] md:h-[45rem] lg:h-[60rem] bg-banner bg-center bg-cover bg-fixed'>
+    <article className='relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem] before:absolute before:content-[""] before:inset-0 before:bg-banner before:bg-center before:bg-cover before:bg-fixed before:will-change-scroll'>
       
     </article>
   )
