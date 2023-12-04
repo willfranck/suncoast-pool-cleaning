136b119cd86cb1d5302f86a53aef9fdc4d5dc52d
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 14:59:56 -0500

    Trying a size hack to prevent resizing when menu pops down

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,4 +1,15 @@
 export default function Banner() {
+  function resizeBackground() {
+    if (typeof document !== 'undefined') {
+      const bannerElement = document.querySelector('.banner') as HTMLElement
+    
+      const currentWidth = parseInt(bannerElement.style.width, 10) || 0
+      bannerElement.style.width = `${currentWidth + 60}px`
+    }
+  }
+  resizeBackground()
+  
+
   return (
     <article className='banner relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem]'>
       
