06e9b0a525d4b9747d63c117c1f94df725733edc
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 13:53:06 -0500

    Reverted will-change property

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <article className='banner relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem] will-change-transform'>
+    <article className='banner relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem]'>
       
     </article>
   )
