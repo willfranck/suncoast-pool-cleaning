23d2474d88c33d3fca6d0447c4e826acdd02331c
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 12:49:02 -0500

    Fix: Moved background to article element

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <article className='w-full h-[35rem] md:h-[45rem] lg:h-[60rem]'>
+    <article className='w-full h-[35rem] md:h-[45rem] lg:h-[60rem] bg-banner bg-center bg-cover bg-fixed'>
       
     </article>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from '@components/footer'
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center min-h-screen bg-banner bg-fixed bg-center bg-cover bg-no-repeat will-change-scroll'>
+    <main className='flex flex-col items-center min-h-screen'>
       <section className='relative flex flex-col items-center w-full min-h-hero bg-hero bg-cover bg-center isolate before:-z-10 before:absolute before:content-[""] before:inset-0 before:bg-tintBlack'>
         <NavBar />
 
