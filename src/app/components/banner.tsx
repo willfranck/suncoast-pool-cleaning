cf06849f10181b9165145332ff3293ca6fd64abc
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 13:38:29 -0500

    Fix: Added component class in globals.css for cleanliness

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <article className='relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem] before:absolute before:content-[""] before:block before:inset-0 before:w-full before:h-full before:bg-banner before:bg-center before:bg-cover before:bg-fixed before:-z-10'>
+    <article className='banner relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem]'>
       <h3>TEST</h3>
     </article>
   )
