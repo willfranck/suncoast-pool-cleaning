37ff7c3dfd17d134c7792ccf8c7cfa07c31cabb1
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-04 14:04:07 -0500

    Removed will-change

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <article className='banner relative w-[100vw] h-[35rem] md:h-[45rem] lg:h-[60rem]'>
+    <article className='banner relative w-full h-[35rem] md:h-[45rem] lg:h-[60rem]'>
       
     </article>
   )
