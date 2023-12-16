4c5c03439500b32da28a4a50877f24ac299a3e2a
willfranck <43352447+willfranck@users.noreply.github.com>
2023-12-16 01:16:20 -0500

    Updated sizing/layout for testimonial cards

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -7,11 +7,11 @@ export default function Testimonials() {
 
 
   return (
-    <article className='grid grid-cols-1 md:grid-cols-2 gap-10 px-6 py-20'>
+    <article className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 px-6 py-20'>
       {testimonials && testimonials.map((testimonial, index) => (
         <div 
           key={index}
-          className='flex flex-col justify-between max-w-[30rem] min-h-[13rem] px-10 py-4 text-black bg-[#59e1e8] rounded-sm shadow-testimonial'
+          className='flex flex-col justify-between w-full max-w-[18rem] min-h-[20rem] px-6 py-4 text-black bg-[#59e1e8] rounded-tr-[15%] rounded-bl-[15%] shadow-testimonial'
         >
           <p className='mb-4'>
             {testimonial.comments}
