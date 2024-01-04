642f3faebc354430b2b45d377ae246d9b59d6554
willfranck <43352447+willfranck@users.noreply.github.com>
2024-01-04 15:17:03 -0500

    Build: Added reveal to testimonials

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { Reveal } from 'react-awesome-reveal'
+import { fadeInFromLeft } from '@utils/keyframes' 
 import { windsong } from '@utils/fonts'
 import content from '@data/content'
 
@@ -7,20 +10,29 @@ export default function Testimonials() {
 
 
   return (
+
     <article className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 px-6 py-20'>
-      {testimonials && testimonials.map((testimonial, index) => (
-        <div 
-          key={index}
-          className='flex flex-col justify-between w-full max-w-[18rem] min-h-[20rem] px-6 py-4 text-black bg-[#59e1e8] rounded-tr-[15%] rounded-bl-[15%] shadow-testimonial'
-        >
-          <p className='mb-4'>
-            {testimonial.comments}
-          </p>
-          <span className={`${windsong.className} text-lg self-end`}>
-            -&ensp;{testimonial.name}
-          </span>
-        </div>
-      ))}
+      <Reveal 
+        keyframes={fadeInFromLeft}
+        fraction={0.4}
+        cascade={true}
+        damping={0.2}
+        triggerOnce={true}
+      >
+       {testimonials && testimonials.map((testimonial, index) => (
+          <div
+            key={index}
+            className='flex flex-col justify-between w-full max-w-[18rem] min-h-[20rem] px-6 py-4 text-black bg-[#59e1e8] rounded-tr-[15%] rounded-bl-[15%] shadow-testimonial'
+          >
+            <p className='mb-4'>
+              {testimonial.comments}
+            </p>
+            <span className={`${windsong.className} text-lg self-end`}>
+              -&ensp;{testimonial.name}
+            </span>
+          </div>
+        ))}
+      </Reveal>
     </article>
   )
 }
